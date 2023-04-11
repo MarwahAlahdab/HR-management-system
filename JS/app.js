21aab58aeaa8e8429a6320d4db3a4fd16a0d4be8
marwasyr <marwaalahdab14@gmail.com>
2023-04-11 12:34:11 +0300

    some updates

--- JS/app.js
+++ JS/app.js
@@ -4,6 +4,7 @@
 let allEmployees=[];
 
 
+
 function Employee(employeeID,fullName,department,level,imageUrl,salary){
 
     this.employeeID=employeeID;
@@ -52,19 +53,62 @@ function Employee(employeeID,fullName,department,level,imageUrl,salary){
   }
 
 
-  Employee.prototype.render = function(){
+  // Employee.prototype.render = function(){
 
-    document.querySelector('main').innerHTML+=(`<main> employee name: ${this.fullName} salary:  ${this.calculateNetSalary(this.salary)} </main>`)
+  //   document.querySelector('main').innerHTML+=(`<main> employee name: ${this.fullName} salary:  ${this.calculateNetSalary(this.salary)} </main>`)
+  // }
+
+
+
+  Employee.prototype.render = function() {
+
+    const container = document.getElementById('cards') 
+    let card = document.createElement('div');
+    card.classList.add('emp-card'); // give a class name
+  
+
+    card.innerHTML = `
+    <img src="${this.imageUrl}"><br>
+      ${this.fullName} - ID:${this.employeeID}<br>
+      <p>Department: ${this.department} - Level: ${this.level}<br>${this.calculateNetSalary(this.salary)}</p>
+    `;
+    container.appendChild(card);
+
+
+
+
+    switch (this.department) {
+      case "Administration":
+        let administration = document.getElementById('Administration');
+        administration.appendChild(card);
+        
+        break;
+      case "Marketing": 
+      let marketing = document.getElementById('Marketing');
+      marketing.appendChild(card);
+        
+        break;
+      case "Development": 
+      let development = document.getElementById('Development');
+      development.appendChild(card);
+        break;
+      case "Finance": 
+      let finance = document.getElementById('Finance');
+      finance.appendChild(card);
+        break;
+    }
+   
+
   }
+  
 
-
-const employee1 = new Employee(1000, "Ghazi Samer", "Administration", "Senior");
-const employee2 = new Employee(1001, "Lana Ali", "Finance", "Senior");
-const employee3 = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior");
-const employee4 = new Employee(1003, "Safi Walid", "Administration", "Mid-Senior");
-const employee5 = new Employee(1004, "Omar Zaid", "Development", "Senior");
-const employee6 = new Employee(1005, "Rana Saleh", "Development", "Junior");
-const employee7 = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior");
+const employee1 = new Employee(1000, "Ghazi Samer", "Administration", "Senior","./assets/149071.png");
+const employee2 = new Employee(1001, "Lana Ali", "Finance", "Senior","./assets/149071.png");
+const employee3 = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior","./assets/149071.png");
+const employee4 = new Employee(1003, "Safi Walid", "Administration", "Mid-Senior","./assets/149071.png");
+const employee5 = new Employee(1004, "Omar Zaid", "Development", "Senior","./assets/149071.png");
+const employee6 = new Employee(1005, "Rana Saleh", "Development", "Junior","./assets/149071.png");
+const employee7 = new Employee(1006, "Hadi Ahmad", "Finance", "Mid-Senior","./assets/149071.png");
 
 
 // employee1.calculateSalary();
@@ -73,3 +117,36 @@ for (let employee of allEmployees) {
    employee.calculateSalary();
   employee.render();
 }
+
+
+
+
+
+
+Employee.prototype.getUniqueId = function() {
+  let counter = 0;
+
+    counter++;
+    let empID = counter.toString().padStart(4, "0");
+    return empID;
+  };
+
+
+let form = document.getElementById("form")
+form.addEventListener('submit',formHandler);
+
+function formHandler (event){
+event.preventDefault();
+let fullName = event.target.fullname.value;
+
+let department = event.target.department.value; 
+let level = event.target.level.value;
+let imageUrl = event.target.imageurl.value;
+
+console.log(fullName,department,level,imageUrl)
+
+}
+
+
+
+
